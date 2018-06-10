20fa60cdba7c0617faf58ed7f3ad2e01853ea4a2
Felipe Hernandez <felipeneo@hotmail.com>
2018-06-09 21:30:55 -0400

    add count variable and for loops to test condition

--- app/1.js
+++ app/1.js
@@ -1,7 +1,27 @@
 const sortByStrings = function(s, t) {
   let sortAs = s.split('');
   let sortBy = t.split('');
-  return (`${sortAs}, ${sortBy}`);
+  let count = 0;
+
+  // loop through each word and compare them
+  for (let i = 0; i < sortAs.length; i += 1) {
+    // store a letter in variable
+    let letter = sortAs[i];
+
+    // second loop will check for each letter
+    for (let i =0; i < sortBy.length; i += 1) {
+      // store a letter in a variable
+      let letter2 = sortBy[i];
+
+      // compare each letter
+      if (letter === letter2) {
+        count += 1;
+      }
+    }
+  }
+
+  return count;
+  //return (`${sortAs}, ${sortBy}`);
 };
 
 const output = sortByStrings('weather', 'car');
