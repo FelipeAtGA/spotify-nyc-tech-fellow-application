db0b60154158aa1b8570fbc506fc53c3b6d98e6f
Felipe Hernandez <felipeneo@hotmail.com>
2018-06-09 23:58:08 -0400

    add comment to first variable declaration; delete count variable; delete empty lines

--- app/1.js
+++ app/1.js
@@ -1,23 +1,18 @@
-let newWord = '';
+let newWord = ''; // this variable collects the desired output
 
 const sortByStrings = function(s, t) {
   let sortAs = s.split('');
   let sortBy = t.split('');
-  let count = 0;
-
   // loop through each word and compare them
   for (let i = 0; i < sortBy.length; i += 1) {
     // store a letter in variable
     const letter = sortBy[i];
-
     // second loop will check for each letter
     for (let i =0; i <= sortAs.length; i += 1) {
       // store a letter in a variable
       const letter2 = sortAs[i];
-
       // compare each letter
       if (letter === letter2) {
-
         // concatenate new word as letters show up
         newWord += letter2;
       }
