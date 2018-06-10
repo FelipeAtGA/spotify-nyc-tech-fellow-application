64c9d4e70bcb6a22ce96997191ca935fe3da7301
Felipe Hernandez <felipeneo@hotmail.com>
2018-06-10 12:26:36 -0400

    move varaible inside loops to top declaration and renamed them

--- app/1.js
+++ app/1.js
@@ -2,20 +2,22 @@ let newWord = ''; // this variable collects the desired output
 
 const sortByStrings = function(s, t) {
   let sortAs = s.split(''),
-      sortBy = t.split('');
+      sortBy = t.split(''),
+      letterSortAS,
+      letterSortBy;
 
   // loop through each word and compare them
   for (let i = 0; i < sortBy.length; i += 1) {
     // store a letter in variable
-    const letter = sortBy[i];
+    letterSortBy = sortBy[i];
     // second loop will check for each letter
     for (let i =0; i <= sortAs.length; i += 1) {
       // store a letter in a variable
-      const letter2 = sortAs[i];
+      letterSortAS = sortAs[i];
       // compare each letter
-      if (letter === letter2) {
+      if (letterSortBy === letterSortAS) {
         // concatenate new word as letters show up
-        newWord += letter2;
+        newWord += letterSortAS;
       }
     }
   }
