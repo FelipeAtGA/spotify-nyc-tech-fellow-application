85f0f4a038460a576bdbbaf8ad31dacbcba97d2a
Felipe Hernandez <felipeneo@hotmail.com>
2018-06-12 13:10:32 -0400

    starting with new strategy; commented out most of old code; change algorithm in decodeString function

--- app/2.js
+++ app/2.js
@@ -1,49 +1,30 @@
 // variable to make an encoded_string
-let tempString = '';
-const charHolder = [];
+let charactersContainer = [],
+    tempString = '';
 
-// create a function that take one parameter
+// make encode string into an array
 const decodeString = function(s) {
-
-  // loop through the string
-  for(let i = 0; i < s.length; i += 1) {
-
-    // assign character to a variable
-    let char = s[i];
-
-    // eliminate brackets
-    if (char !== '[' && char !== ']') {
-
-      // separate first k times
-      if ( !isNaN(char) ) {
-
-        // pupulate array
-        charHolder.push(char);
-        // console.log (i,char);
-      } else if ( char.match(/^[A-Za-z]+$/) ) {
-        tempString += char;
-        // console.log(i,tempString);
-      }
-    }
-  }
+  // use regular expression match statement
+  charactersContainer = s.match(/[a-zA-Z]+|[0-9]/g);
+  console.log(charactersContainer);
 };
 
-const createEncodedString = function(arr) {
-  // holds the encoded string
-  let encodedString = '';
+// const createEncodedString = function(arr) {
+//   // holds the encoded string
+//   let encodedString = '';
 
-  // loop over the array
-  for (let i = 0; i < arr[0]; i+= 1) {
-    encodedString += arr[1];
-  }
+//   // loop over the array
+//   for (let i = 0; i < arr[0]; i+= 1) {
+//     encodedString += arr[1];
+//   }
 
-  console.log(encodedString);
-}
+//   console.log(encodedString);
+// }
 
 decodeString('4[ab]');
 
 // add the made encoded_string to array
-charHolder.push(tempString);
-createEncodedString(charHolder);
+// charHolder.push(tempString);
+// createEncodedString(charHolder);
 
 export default decodeString;
