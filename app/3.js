46228cf2344cc22e0bdd24422ddfec532a2c3aa9
Felipe Hernandez <felipeneo@hotmail.com>
2018-06-14 13:37:37 -0400

    add initial for loop and while loop; also function call

--- app/3.js
+++ app/3.js
@@ -4,4 +4,14 @@ const changePossibilities = function(amount, arr) {
   let result = 0, // holds final result
       value  = 0, // coin denomination
       index  = 0; // index of array
+
+  // iterates through array of coins
+  for( let i=0; i < arr.length; i += 1 ) {
+    // iterates through all possible values under the amount
+    while ( value <= amount ) {
+      console.log(value);
+    }
+  }
 }
+
+changePossibilities(4, [1,2,3]);
