fc24c69585bb870c7e1a8030ed21c332ff8eb916
Felipe Hernandez <felipeneo@hotmail.com>
2018-06-14 13:30:36 -0400

    create function chnagePossibilities and initial variables

---
+++ app/3.js
@@ -0,0 +1,7 @@
+// functions computes the number of ways to make the
+// amount of money with coins of the available denominations
+const changePossibilities = function(amount, arr) {
+  let result = 0, // holds final result
+      value  = 0, // coin denomination
+      index  = 0; // index of array
+}
