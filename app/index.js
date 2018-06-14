36486d7d93ea056fc2b510f8c3df24fe63d9bd4f
Felipe Hernandez <felipeneo@hotmail.com>
2018-06-14 13:39:43 -0400

    import file 3 changePossifilities function into index.js

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 console.log('webpack up and running');
 
-import sortByStrings from './1';
-import decodeString  from './2';
+import sortByStrings       from './1';
+import decodeString        from './2';
+import changePossibilities from './3';
